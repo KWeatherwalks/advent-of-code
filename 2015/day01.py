fc1b344a690aedc3a390b09abc537c22db0965c1
KWeatherwalks <kevin.weatherwalks@gmail.com>
2023-11-28 21:50:30 -0700

    solve and submit day 1

--- 2015/day01.py
+++ 2015/day01.py
@@ -7,17 +7,24 @@ from aocd.models import Puzzle
 
 def parse(puzzle_input):
     """Parse input"""
-    return puzzle_input.split("\n")
+    return puzzle_input
 
 
 def part1(data):
     """Solve part 1"""
-    return
+    return data.count("(") - data.count(")")
 
 
 def part2(data):
     """Solve part 2"""
-    return
+    counts = [0, 0]
+    for position, p in enumerate(data, start=1):
+        if p == "(":
+            counts[0] += 1
+        if p == ")":
+            counts[1] += 1
+        if counts[0] < counts[1]:
+            return position
 
 
 def solve(puzzle_input):
@@ -31,9 +38,9 @@ def solve(puzzle_input):
 
 if __name__ == "__main__":
     # Connect to the API
-    puzzle_input = Puzzle(year=2022, day=None).input_data
+    puzzle_input = Puzzle(year=2015, day=1).input_data
     # Solve
     answer1, answer2 = solve(puzzle_input)
     # Submit answers
-    submit(answer1, part='a', day=None, year=2022)
-    submit(answer2, part='b', day=None, year=2022)
+    submit(answer1, part="a", day=1, year=2015)
+    submit(answer2, part="b", day=1, year=2015)
--- 2015/test_day01.py
+++ 2015/test_day01.py
@@ -10,7 +10,7 @@ from aocd.models import Puzzle
 PUZZLE_DIR = pathlib.Path(__file__).parent / "example-data"
 
 
-def parse_text_file(filename):
+def txt_to_string(filename):
     """Helper function to keep fixture functions DRY"""
     return (PUZZLE_DIR / filename).read_text().strip()
 
@@ -18,55 +18,62 @@ def parse_text_file(filename):
 # Fixtures
 @pytest.fixture
 def example1():
-    puzzle_input = parse_text_file("example1.txt")
+    puzzle_input = txt_to_string("01_example1.txt")
+    print(puzzle_input)
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example2():
-    puzzle_input = parse_text_file("example2.txt")
+    puzzle_input = txt_to_string("01_example2.txt")
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example3():
-    puzzle_input = parse_text_file("example3.txt")
+    puzzle_input = txt_to_string("01_example3.txt")
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example4():
-    puzzle_input = parse_text_file("example4.txt")
+    puzzle_input = txt_to_string("01_example4.txt")
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example5():
-    puzzle_input = parse_text_file("example5.txt")
+    puzzle_input = txt_to_string("01_example5.txt")
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example6():
-    puzzle_input = parse_text_file("example6.txt")
+    puzzle_input = txt_to_string("01_example6.txt")
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example7():
-    puzzle_input = parse_text_file("example7.txt")
+    puzzle_input = txt_to_string("01_example7.txt")
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example8():
-    puzzle_input = parse_text_file("example8.txt")
+    puzzle_input = txt_to_string("01_example8.txt")
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example9():
-    puzzle_input = parse_text_file("example9.txt")
+    puzzle_input = txt_to_string("01_example9.txt")
+    return aoc.parse(puzzle_input)
+
+
+@pytest.fixture
+def example10():
+    puzzle_input = txt_to_string("01_example10.txt")
     return aoc.parse(puzzle_input)
 
 
@@ -76,6 +83,7 @@ def example9():
 def test_parse_example1(example1):
     """Test that input is parsed properly"""
     expected = "(())"
+    assert type(example1) == type(expected)
     assert example1 == expected
 
 
@@ -130,7 +138,50 @@ def test_part1_example9(example9):
 # Part 2
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1):
     """Test part 2 on example input"""
-    assert aoc.part2(example1) == ...
+    assert aoc.part2(example1) == None
+
+
+def test_part2_example2(example2):
+    """Test part 2 on example input"""
+    assert aoc.part2(example2) == None
+
+
+def test_part2_example3(example3):
+    """Test part 2 on example input"""
+    assert aoc.part2(example3) == None
+
+
+def test_part2_example4(example4):
+    """Test part 2 on example input"""
+    assert aoc.part2(example4) == None
+
+
+def test_part2_example5(example5):
+    """Test part 2 on example input"""
+    assert aoc.part2(example5) == 1
+
+
+def test_part2_example6(example6):
+    """Test part 2 on example input"""
+    assert aoc.part2(example6) == 3
+
+
+def test_part2_example7(example7):
+    """Test part 2 on example input"""
+    assert aoc.part2(example7) == 1
+
+
+def test_part2_example8(example8):
+    """Test part 2 on example input"""
+    assert aoc.part2(example8) == 1
+
+
+def test_part2_example9(example9):
+    """Test part 2 on example input"""
+    assert aoc.part2(example9) == 1
+
+
+def test_part2_example10(example10):
+    assert aoc.part2(example10) == 5
